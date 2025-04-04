2ec366757838e22e6fe037ecd0bc1bdfcc0169bb
FenrirKin <jarfjinq@students.codeinstitute.net>
2025-04-04 16:39:37 +0100

    Add board creation to script.js

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ let seconds = 0;
 let minutes = 0;
 let isGameOver = false;
 let gridSize = 4; // Default to 4x4 (medium)
+
+function createBoard() {
+        gameBoard.innerHTML = '';
+        const cardValues = generateCards();
+      
+        cardValues.forEach((value, index) => {
+          const cardElement = document.createElement('div');
+          cardElement.classList.add('card');
+          cardElement.dataset.index = index;
+          cardElement.dataset.value = value;
+          cardElement.addEventListener('click', flipCard);
+          gameBoard.appendChild(cardElement);
+        });
+      
+        gameBoard.style.gridTemplateColumns = `repeat(${gridSize}, 100px)`;
+      }
