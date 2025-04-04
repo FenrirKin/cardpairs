d5b98c61787632f9b49a7e225e89d81d16be149a
FenrirKin <jarfjinq@students.codeinstitute.net>
2025-04-04 16:41:18 +0100

    Add shuffle and generate function to board creation

--- script.js
+++ script.js
@@ -15,6 +15,24 @@ let minutes = 0;
 let isGameOver = false;
 let gridSize = 4; // Default to 4x4 (medium)
 
+function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+      }
+
+function generateCards() {
+        const numPairs = (gridSize * gridSize) / 2;
+        const cardValues = [];
+        for (let i = 0; i < numPairs; i++) {
+          cardValues.push(String.fromCharCode(65 + i), String.fromCharCode(65 + i)); // A, B, C, etc.
+        }
+        return shuffle(cardValues);
+      }
+      
+
 function createBoard() {
         gameBoard.innerHTML = '';
         const cardValues = generateCards();
